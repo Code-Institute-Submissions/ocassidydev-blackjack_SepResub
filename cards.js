97001814093dd9159dab2298d4300a84b14637de
ocassidydev <ocassidy@tcd.ie>
2022-07-05 16:23:05 +0000

    Add playCard function

--- cards.js
+++ cards.js
@@ -1,12 +1,15 @@
+// function for randomly sorting the array of cards
 function shuffle(arr) {
     arr.sort(() => Math.random() - 0.5);
 }
 
+// class for defining card objects
 class card {
     constructor(suit,value) {
         switch(value) {
             case 1:
                 this.id = "ace";
+                // ace can have value of 1 or 11, this function is to handle this
                 this.value = function(aceBool, value){
                     if(aceBool) {
                     return 11; 
@@ -36,9 +39,31 @@ class card {
     }
 };
 
+// function that adds a card to the table
+function playCard(deck, table, aceBool){
+    let cardVal;
+    
+    table.push(deck[0]);
+
+    if(len(deck[0].id) > 3){
+        aceBool = true;
+    }
+
+    if(deck[0].id === "ace"){
+        cardVal = deck[0].value(aceBool);
+    }
+    else{
+        cardVal = deck[0].value;
+    }
+
+    deck.shift();
+    return(cardVal)
+}
+
 let suits = ["clubs","spades","diamonds","hearts"];
 let deck = [];
 
+// for loop to populate deck
 for (let i of suits){
     for (let j = 1; j<14; j++){
         let newCard = new card(i,j);
@@ -47,4 +72,18 @@ for (let i of suits){
 };
 
 shuffle(deck);
-console.log(deck);
+
+let playerChips = 10000;
+let playerTable = [];
+let playerVal = 0;
+let aceBool = false;
+
+// Plays first 2 
+for(let i = 0; i < 2; i++){
+    playerVal += playCard(deck, playerTable, aceBool);
+}
+
+while(playerVal<21){
+
+}
+
